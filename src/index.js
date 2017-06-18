a85ad266d7c67042af824f02dd72a4fc53e56313
linkcd <linkcd@gmail.com>
2017-06-18 20:33:45 +0200

    added express and pm2

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-import Dog from './dog'
-
-const str = 'ES6'
-console.log(`Hello ${str}`)
-
-const toby = new Dog('Toby')
-
-console.log(toby.bark())
---
+++ src/server/render-app.js
@@ -0,0 +1,16 @@
+import { STATIC_PATH } from '../shared/config'
+
+const renderApp = () =>
+  `<!doctype html>
+        <html>
+        <head>
+            <title>xxx</title>
+            <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
+        </head>
+        <body>
+            <h1>yyyy!!!!!!!!!</h1>
+        </body>
+        </html>
+    `
+
+export default renderApp
